a8e6f6502b9cfce46b7f3ae623277c00952163a8
Maria Karpukhina <myshka_n@mail.ru>
2021-10-13 22:41:11 +0300

    Add models

--- CarChekUp/Auto.swift
+++ CarChekUp/Auto.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct Auto {
+    let title: String
+    let brand: String
+    let model: String
+    let currentMileage: Int
+    let averageMileage: Int
+    var description: String {
+        "\(brand) \(model)"
+    }
+}
+
+struct TypeOfWork {
+    let title: String
+    let isPeriodic: Bool
+    let mileage: Int
+}
+
+struct CarriedWork {
+    let work: TypeOfWork
+    let date: Date
+    let cost: Int
+}
